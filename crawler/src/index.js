bed54f6b7538baac59029c5c63bd9710fcd7ec1d
Mark van Straten <mark.van.straten@nn.nl>
2021-03-06 15:58:31 +0100

    create date stamped index on start (for now)

--- crawler/src/index.js
+++ crawler/src/index.js
@@ -3,14 +3,13 @@ const figlet = require('figlet');
 const puppeteer = require('puppeteer');
 const $ = require('cheerio');
 const { Client } = require('@elastic/elasticsearch')
-const client = new Client({ node: 'http://localhost:9200' })
 const normalizeUrl = require('normalize-url');
 const Rx = require('rxjs');
 const RxOp = require('rxjs/operators');
 //import { queueScheduler, Subject } from 'rxjs';
 
 
-console.log(chalk.yellow(figlet.textSync('Web Crawler', {horizontalLayout: 'full' })));
+console.log(chalk.yellow(figlet.textSync('Web Crawler', { horizontalLayout: 'full' })));
 
 const argv = require('minimist')(process.argv.slice(2));
 
@@ -22,12 +21,15 @@ if (argv.url === undefined || argv.whitelisturls === undefined) {
 mainAsync();
 
 async function mainAsync() {
+    const client = new Client({ node: 'http://localhost:9200' })
+
     const url = normalizeUrl(argv.url);
     const baseUrl = url; // starting point, we do not escape from this domain
 
-    console.log(`Going to crawl root site ${url}`);
-    process.env.NODE_TLS_REJECT_UNAUTHORIZED=0  
+    console.log(`Going to crawl root site ${url}...`);
+    process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0
 
+    const indexName = await createIndex(client);
     const seenUrls = [];
 
     const browser = await puppeteer.launch()
@@ -36,19 +38,19 @@ async function mainAsync() {
         RxOp.map((url) => normalizeUrl(url)),
         RxOp.filter((url) => !seenUrls.find(seenUrl => seenUrl === url)),
         RxOp.concatMap((url) => fetchAndParsePage(browser, url)),
-        RxOp.mergeMap((parsedPageObject) => index(parsedPageObject)),
-        RxOp.tap((result) => { 
-            seenUrls.push(result.url); 
+        RxOp.mergeMap((parsedPageObject) => index(indexName, parsedPageObject)),
+        RxOp.tap((result) => {
+            seenUrls.push(result.url);
             // only go deeper with our crawl on the same domain
-            result.urls
+            result.outbound_urls
                 .filter(u => u.startsWith(baseUrl))
                 .forEach(u => queue.next(u));
         })
     )
-    .subscribe(
-        (result) => console.log('downloaded and indexed url: ' + result.url),
-        (err) => console.error('Error: ' + err)
-    );
+        .subscribe(
+            (result) => console.log('downloaded and indexed url: ' + result.url),
+            (err) => console.error('Error: ' + err)
+        );
 
     queue.next(url);
 }
@@ -82,12 +84,11 @@ async function parsePageContents(html, url) {
     });
 
     const parsed = {
-        content: html,
+        content: $('body', html).text(),
         title: $('title', html).text(),
         url: url,
-        domain: 'something-todo',
         crawled_at: new Date().toISOString(),
-        urls: urls.map(u => normalizeUrl(u)).filter(onlyUnique), //dedupe and normalize
+        outbound_urls: urls.map(u => normalizeUrl(u)).filter(onlyUnique), //dedupe and normalize
     };
     return parsed;
 }
@@ -98,13 +99,13 @@ function onlyUnique(value, index, self) {
 }
 
 // index the given page object to our search engine
-async function index(pageObject) {
+async function index(indexName, pageObject) {
     console.log('going to index', pageObject.url);
     // index it!
-    try {    
+    try {
         //await Promise.resolve('FOO');
         const indexResult = await client.index({
-            index: 'webpages',
+            index: indexName,
             body: { ...pageObject }
         });
         // console.log('indexed into ES', indexResult);
@@ -113,4 +114,68 @@ async function index(pageObject) {
         console.log('error while indexing', error);
     }
     return pageObject; // chain the object for easy usage
+}
+
+
+
+async function createIndex(client) {
+    // create index with mapping
+    // return indexname
+    function formatDate(date) {
+        return [
+            date.getFullYear(),
+            date.getMonth(),
+            date.getDay(),
+            date.getHours(),
+            date.getMinutes()
+        ]
+            .map(s => ('' + s).padStart(2, '0'))
+            .join('');
+    }
+
+    const indexName = `webpages-${formatDate(new Date())}`;
+
+    // for debugging elasticsearch error responses
+    // client.on('response', (err, result) => {
+    //     console.log(err, result)
+    // })
+
+
+    console.log(`Creating index... ${indexName}`);
+    await client.indices.create({
+        index: indexName,
+        body: {
+            settings: {
+                index: {
+                    number_of_shards: 2,
+                    number_of_replicas: 0 // for local development
+                },
+                analysis: {
+                    analyzer: {
+                        htmlstrip_analyzer: {
+                            tokenizer: 'standard',
+                            char_filter: ['html_strip'],
+                            filter: ['lowercase', 'asciifolding']
+                        }
+                    }
+                },
+            },
+            mappings: {
+                properties: {
+                    content: {
+                        type: 'text',
+                        analyzer: 'htmlstrip_analyzer'
+                    },
+                    title: { type: 'text' },
+                    url: { type: 'keyword', },
+                    crawled_at: { type: 'date' },
+                    outbound_urls: { type: 'keyword', },
+                }
+            }
+        }
+    });
+
+    console.log(`Done preparing new ES index: ${indexName}`);
+
+    return indexName;
 }
