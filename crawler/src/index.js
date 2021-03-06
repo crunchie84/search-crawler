c69f8a68a43bfed078f71ae1deec368a9ae7318b
Mark van Straten <mark.van.straten@nn.nl>
2021-03-06 12:40:24 +0100

    crawler works on same domain now

--- crawler/src/index.js
+++ crawler/src/index.js
@@ -5,6 +5,9 @@ const $ = require('cheerio');
 const { Client } = require('@elastic/elasticsearch')
 const client = new Client({ node: 'http://localhost:9200' })
 const normalizeUrl = require('normalize-url');
+const Rx = require('rxjs');
+const RxOp = require('rxjs/operators');
+//import { queueScheduler, Subject } from 'rxjs';
 
 
 console.log(chalk.yellow(figlet.textSync('Web Crawler', {horizontalLayout: 'full' })));
@@ -16,14 +19,11 @@ if (argv.url === undefined || argv.whitelisturls === undefined) {
     process.exit(1);
 }
 
- 
-
-
-mainAsync()
-.then(() => console.log('done...'), (err) => console.error('error: '+err));
+mainAsync();
 
 async function mainAsync() {
     const url = normalizeUrl(argv.url);
+    const baseUrl = url; // starting point, we do not escape from this domain
 
     console.log(`Going to crawl root site ${url}`);
     process.env.NODE_TLS_REJECT_UNAUTHORIZED=0  
@@ -31,56 +31,36 @@ async function mainAsync() {
     const seenUrls = [];
 
     const browser = await puppeteer.launch()
-    const parsed = await fetchPage(browser, url);
-    await index(parsed);
-    seenUrls.push(url);
+    const queue = new Rx.Subject();
+    queue.pipe(
+        RxOp.map((url) => normalizeUrl(url)),
+        RxOp.filter((url) => !seenUrls.find(seenUrl => seenUrl === url)),
+        RxOp.concatMap((url) => fetchAndParsePage(browser, url)),
+        RxOp.mergeMap((parsedPageObject) => index(parsedPageObject)),
+        RxOp.tap((result) => { 
+            seenUrls.push(result.url); 
+            // only go deeper with our crawl on the same domain
+            result.urls
+                .filter(u => u.startsWith(baseUrl))
+                .forEach(u => queue.next(u));
+        })
+    )
+    .subscribe(
+        (result) => console.log('downloaded and indexed url: ' + result.url),
+        (err) => console.error('Error: ' + err)
+    );
+
+    queue.next(url);
 }
 
-async function fetchPage(browser, url) {
-    const page = browser.newPage();
+
+
+
+async function fetchAndParsePage(browser, url) {
+    console.log('Going to fetch url: ', url);
+    const page = await browser.newPage();
     const html = await page.goto(url).then(() => page.content());
-    const parsed = await parsePageContents(html);
-    return parsed;
-}
-
-
-
-async function crawlAndIndex(url, urlsIndexed) {
-    const result = await crawl(url);
-    const parsed = await parsePageContents(result.pageContents);
-    await index({ 
-        url: result.pageUrl,
-        domain: result.domain,
-        title: parsed.title,
-        content: parsed.content
-    });
-
-    // this url is also indexed
-    urlsIndexed.push(url);
-
-    // only return the urls which have not yet been indexed
-    const notYetIndexedUrls = parsed.urls.filter(u => !urlsIndexed.any(ui => ui === u));
-
-    return {
-        pageUrls: notYetIndexedUrls,
-        urlsIndexed
-    };
-}
-
-/**
- * crawl the given page, return its contents and extract urls
- * @param {} pageUrl 
- * @param {*} whitelistedDomains 
- */
-async function crawl(pageUrl) {
-
-    // parse domain from the url
-
-    return {
-        url: pageUrl,
-        domain: '',
-        pageContents: ''
-    }
+    return await parsePageContents(html, url);
 }
 
 // take the page contents and extract any follow up urls to crawl
@@ -95,31 +75,42 @@ async function parsePageContents(html, url) {
         }
 
         if (!href.startsWith('https://')) {
-            return;
+            return; // only extract https page urls for now, skip stuff like 'mailto
         }
+
         urls.push(href);
     });
 
-    return {
+    const parsed = {
         content: html,
         title: $('title', html).text(),
-        url,
+        url: url,
+        domain: 'something-todo',
         crawled_at: new Date().toISOString(),
-        urls,
-    }
+        urls: urls.map(u => normalizeUrl(u)).filter(onlyUnique), //dedupe and normalize
+    };
+    return parsed;
+}
+
+// source from https://stackoverflow.com/a/14438954/106909
+function onlyUnique(value, index, self) {
+    return self.indexOf(value) === index;
 }
 
 // index the given page object to our search engine
 async function index(pageObject) {
+    console.log('going to index', pageObject.url);
     // index it!
-    try{
+    try {    
+        //await Promise.resolve('FOO');
         const indexResult = await client.index({
             index: 'webpages',
             body: { ...pageObject }
         });
-        console.log('indexed into ES', indexResult);
+        // console.log('indexed into ES', indexResult);
     }
     catch (error) {
-        console.log('error while indexing', error);   
+        console.log('error while indexing', error);
     }
+    return pageObject; // chain the object for easy usage
 }
